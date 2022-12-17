f2bbb4abbc18ff4bf92046e9f129bfeace602f4c
Andrea Grandieri <g.andreus02@gmail.com>
2022-12-17 23:07:46 +0100

    Test

--- engine/js/global.js
+++ engine/js/global.js
@@ -10,41 +10,46 @@
 
 // This is a module. The globalThis export is used.
 
-// Global variables
-var theme = "light"
-
 // Function to switch between light and dark mode
 // The function should be called with a button
 function toggleDarkMode() {
-    // If theme is dark, switch to light
-    // If theme is light, switch to dark
-    // Default theme is "light"
+  // If theme is dark, switch to light
+  // If theme is light, switch to dark
+  // Default theme is "light"
 
-    // // // Using "javascript static variable" way of declaration
-    // // if (typeof toggleDarkMode.theme === 'undefined') {
-    // //   toggleDarkMode.theme = "light"
-    // // }
-
-    // // if (toggleDarkMode.theme == "light") {
-    // //   // Change button with id "lighdarkSwitcherButton" text
-    // //   document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
-    // //   toggleDarkMode.theme = "dark"
-    // // } else {
-    // //   document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
-    // //   toggleDarkMode.theme = "light"
-    // // }
-
-    // // jtd.setTheme(toggleDarkMode.theme)
-
-    if (theme == "light") {
-      // Change button with id "lighdarkSwitcherButton" text
-      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
-      theme = "dark"
-    } else {
-      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
-      theme = "light"
+  // Tries with localStorage first
+  if (typeof (Storage) !== "undefined") {
+    // Using "javascript static variable" way of declaration
+    if (typeof localStorage.theme === 'undefined') {
+      localStorage.theme = "light"
     }
 
-    jtd.setTheme(theme)
+    if (localStorage.theme == "light") {
+      // Change button with id "lighdarkSwitcherButton" text
+      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
+      localStorage.theme = "dark"
+    } else {
+      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
+      localStorage.theme = "light"
+    }
+
+    jtd.setTheme(localStorage.theme)
+  } else {
+    // Using "javascript static variable" way of declaration
+    if (typeof toggleDarkMode.theme === 'undefined') {
+      toggleDarkMode.theme = "light"
+    }
+
+    if (toggleDarkMode.theme == "light") {
+      // Change button with id "lighdarkSwitcherButton" text
+      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
+      toggleDarkMode.theme = "dark"
+    } else {
+      document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
+      toggleDarkMode.theme = "light"
+    }
+
+    jtd.setTheme(toggleDarkMode.theme)
+  }
 }
 globalThis.toggleDarkMode = toggleDarkMode
