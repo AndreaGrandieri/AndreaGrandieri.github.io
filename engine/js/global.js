9d01810ae46298bde10dcc9c55020c76282d150b
Andrea Grandieri <g.andreus02@gmail.com>
2022-12-18 00:06:40 +0100

    Update global.js

--- engine/js/global.js
+++ engine/js/global.js
@@ -11,22 +11,38 @@
 // This is a module. The globalThis export is used.
 
 // Function to switch between light and dark mode
-// The function should be called with a button
+// The function should be called within a button
 function themeModeSwitcher() {
   // If theme is dark, switch to light
   // If theme is light, switch to dark
   // Default theme is "light"
   // It implements the continuity of the theme between pages, using sessionStorage
+  // Fallback is implemented if sessionStorage is not supported by the browser
 
-  if (sessionStorage.theme) {
-    if (sessionStorage.theme == "light") {
-      toggleDarkMode()
+  // Checks if the "sessionStorage" object is supported by the browser
+  if (typeof (Storage) !== "undefined") {
+    if (sessionStorage.theme) {
+      if (sessionStorage.theme == "light") {
+        toggleDarkMode()
+      } else {
+        toggleLightMode()
+      }
     } else {
-      toggleLightMode()
+      sessionStorage.theme = "light"
+      toggleDarkMode()
     }
   } else {
-    sessionStorage.theme = "light"
-    toggleDarkMode()
+    // Continuity won't work
+    if (themeModeSwitcher.theme !== "undefined") {
+      if (themeModeSwitcher.theme == "light") {
+        toggleDarkMode()
+      } else {
+        toggleLightMode()
+      }
+    } else {
+      themeModeSwitcher.theme = "light"
+      toggleDarkMode()
+    }
   }
 }
 globalThis.themeModeSwitcher = themeModeSwitcher
@@ -34,26 +50,51 @@ globalThis.themeModeSwitcher = themeModeSwitcher
 function toggleDarkMode() {
   document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
 
-  sessionStorage.theme = "dark"
-  jtd.setTheme(sessionStorage.theme)
-}
-globalThis.toggleDarkMode = toggleDarkMode
+  // Checks if the "sessionStorage" object is supported by the browser
+  if (typeof (Storage) !== "undefined") {
+    sessionStorage.theme = "dark"
+    jtd.setTheme(sessionStorage.theme)
+  } else {
+    // Continuity won't work
+    themeModeSwitcher.theme = "dark"
+    jtd.setTheme(themeModeSwitcher.theme)
+  }
+  globalThis.toggleDarkMode = toggleDarkMode
 
-function toggleLightMode() {
-  document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
+  function toggleLightMode() {
+    document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
 
-  sessionStorage.theme = "light"
-  jtd.setTheme(sessionStorage.theme)
-}
-globalThis.toggleLightMode = toggleLightMode
-
-function retrieveTheme() {
-  if (sessionStorage.theme) {
-    if (sessionStorage.theme == "light") {
-      toggleLightMode()
+    // Checks if the "sessionStorage" object is supported by the browser
+    if (typeof (Storage) !== "undefined") {
+      sessionStorage.theme = "light"
+      jtd.setTheme(sessionStorage.theme)
     } else {
-      toggleDarkMode()
+      // Continuity won't work
+      themeModeSwitcher.theme = "light"
+      jtd.setTheme(themeModeSwitcher.theme)
     }
   }
-}
-globalThis.retrieveTheme = retrieveTheme
+  globalThis.toggleLightMode = toggleLightMode
+
+  function retrieveTheme() {
+    // Checks if the "sessionStorage" object is supported by the browser
+    if (typeof (Storage) !== "undefined") {
+      if (sessionStorage.theme) {
+        if (sessionStorage.theme == "light") {
+          toggleLightMode()
+        } else {
+          toggleDarkMode()
+        }
+      }
+    } else {
+      // Continuity won't work
+      if (themeModeSwitcher.theme !== "undefined") {
+        if (themeModeSwitcher.theme == "light") {
+          toggleLightMode()
+        } else {
+          toggleDarkMode()
+        }
+      }
+    }
+  }
+  globalThis.retrieveTheme = retrieveTheme
