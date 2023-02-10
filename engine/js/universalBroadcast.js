3436d636e53592dc54218316472709d0ad8d2c50
Andrea Grandieri <g.andreus02@gmail.com>
2023-02-10 22:53:15 +0100

    RepoLink version B: site assets updated

--- engine/js/universalBroadcast.js
+++ engine/js/universalBroadcast.js
@@ -31,7 +31,20 @@ import * as vars_universalBroadcast from "./vars_universalBroadcast.js";
 }
 */
 
+// News schema is in the following format:
+/*
+{
+    "callout_levels": [
+        {
+            "callout_level": "",
+            "content": ""
+        }
+    ]
+}
+*/
+
 var news = null;
+var newsSchema = null;
 
 // Query the CDN for news to be broadcasted
 async function getNewsFromCDN() {
@@ -51,17 +64,37 @@ async function getNewsFromCDN() {
 }
 globalThis.getNewsFromCDN = getNewsFromCDN;
 
+async function getNewsSchemaFromCDN() {
+    // Check if "newsSchema" is not null or undefined: if so, the news schema have already been fetched and there is no need to fetch them again
+    if (newsSchema == null || newsSchema == undefined) {
+        try {
+            // "queryCDN" uses "JSON.parse" already
+            await CDNQuerierEngine.queryCDN(vars_universalBroadcast.newsSchema, function (data) {
+                newsSchema = data;
+            });
+        } catch (e) {
+            // Error. Handling:
+            globalShared.toggle_engine_fetching_inErrorState();
+            return;
+        }
+    }
+}
+globalThis.getNewsSchemaFromCDN = getNewsSchemaFromCDN;
+
 // Broadcast the news
 async function broadcastNews() {
-    // First of all, get the news
+    // First of all, get the news and the schema
     try {
+        await getNewsSchemaFromCDN();
         await getNewsFromCDN();
 
         // Now, "news" contains all the news to be broadcasted.
         // Traverse all the news following the format above
         for (var i = 0; i < news.news.length; i++) {
             // Check the "validityURL" of the news: the news should only be displayed if "validityURL" matches the current URL of the page
-            if (news.news[i].validityURL == window.location.href) {
+
+            // Check if the "validityURL" is the current URL of the page, regardless of the presence of ".html" at the end of the URL of the current page
+            if (news.news[i].validityURL == window.location.href.replace(".html", "")) {
                 // Parse the date in "birthday". Example format: "1.Jan.2021"
                 var date = new Date(news.news[i].birthday);
 
@@ -74,8 +107,30 @@ async function broadcastNews() {
                 // Check if "date" falls after "currentDate": if so, the news SHOULD NOT be displayed
                 if (currentDate <= date) {
                     // News to be displayed
-                    console.log(news.news[i].title);
-                    console.log(news.news[i].content);
+                    var toInject = "";
+
+                    // From "newsSchema", get the "content" based on "callout_level" from "news"
+                    for (var j = 0; j < newsSchema.callout_levels.length; j++) {
+                        if (newsSchema.callout_levels[j].callout_level == news.news[i].callout_level) {
+                            toInject += newsSchema.callout_levels[j].content;
+                            break;
+                        }
+                    }
+
+                    // A title is present if "title" is not null or undefined
+                    if (news.news[i].title != null && news.news[i].title != undefined) {
+                        // Append the title to "toInject", the close the "p" tag
+                        toInject += news.news[i].title + "</p>";
+                    }
+
+                    // Open the "p" tag and append the content to "toInject"
+                    toInject += "<p>" + news.news[i].content;
+
+                    // Close "blockquote"
+                    toInject += "</blockquote>";
+
+                    // "toInject" is now ready to be injected into the DOM. Inject in the div with id "broadcastTarget_universalBroadcast"
+                    document.getElementById("broadcastTarget_universalBroadcast").innerHTML = toInject;
                 }
 
                 continue;
@@ -83,7 +138,7 @@ async function broadcastNews() {
 
             // Check the "validityURL" of the news: check if the URL is the base URL of the website.
             // Check if the "validityURL" is a substring of the current URL of the page
-            if (news.news[i].validityURL.includes(window.location.href)) {
+            if (news.news[i].validityURL.includes(window.location.origin)) {
                 console.log("ORIGIIIIIIN");           
             }
         }
--- engine/js/vars_universalBroadcast.js
+++ engine/js/vars_universalBroadcast.js
@@ -11,3 +11,5 @@
 
 // Variables to be used by "universalBroadcast.js"
 export const news = ["https://raw.githubusercontent.com/AndreaGrandieri/andreagrandieri.github.io/cdn/universalBroadcast.json"];
+
+export const newsSchema = ["https://raw.githubusercontent.com/AndreaGrandieri/andreagrandieri.github.io/cdn/universalBroadcastSchema.json"]
